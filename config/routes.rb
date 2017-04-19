877d4a882c2b877de7856b5d18d075b4510003d0
flas122 <xingkxdha@gmail.com>
2017-04-19 16:49:20 +0800

    when created a group, join it automatically

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
     resources :groups do
 
-      member do
-      post :join
-      post :quit
-    end
+            member do
+            post :join
+            post :quit
+          end
 
       resources :posts
     end
